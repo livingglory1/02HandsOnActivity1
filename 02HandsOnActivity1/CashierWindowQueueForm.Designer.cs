3a450980885e7bff4476b39662ce8871df2833a1
Rupert Duazo <rupertduazo02@gmail.com>
2020-10-23 18:28:39 -0700

    Initial Commit

---
+++ 02HandsOnActivity1/CashierWindowQueueForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace _02HandsOnActivity1
+{
+    partial class CashierWindowQueue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.listCashierQueue = new System.Windows.Forms.ListBox();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(13, 13);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(83, 30);
+            this.btnRefresh.TabIndex = 0;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // listCashierQueue
+            // 
+            this.listCashierQueue.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listCashierQueue.FormattingEnabled = true;
+            this.listCashierQueue.ItemHeight = 18;
+            this.listCashierQueue.Location = new System.Drawing.Point(102, 13);
+            this.listCashierQueue.Name = "listCashierQueue";
+            this.listCashierQueue.Size = new System.Drawing.Size(182, 274);
+            this.listCashierQueue.TabIndex = 2;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(12, 49);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(83, 30);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
+            this.label1.Location = new System.Drawing.Point(12, 274);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "*Waiting Time";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(12, 244);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(11, 13);
+            this.lblTime.TabIndex = 5;
+            this.lblTime.Text = "*";
+            // 
+            // CashierWindowQueue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 302);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.listCashierQueue);
+            this.Controls.Add(this.btnRefresh);
+            this.Name = "CashierWindowQueue";
+            this.Text = "CashierWindowQueueForm";
+            this.Load += new System.EventHandler(this.CashierWindowQueue_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.ListBox listCashierQueue;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTime;
+    }
+}
---
+++ 02HandsOnActivity1/CashierWindowQueueForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02HandsOnActivity1
+{
+    public partial class CashierWindowQueue : Form
+    {
+        public CashierWindowQueue()
+        {
+            InitializeComponent();
+        }
+
+        private int _tick;
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            DisplayCashierQueue(CashierClass.CashierQueue);
+            timer1.Stop();
+        }
+        public void DisplayCashierQueue(IEnumerable CashierList)
+        {
+            listCashierQueue.Items.Clear();
+            foreach(object obj in CashierList)
+            {
+                listCashierQueue.Items.Add(obj.ToString());
+            }
+        }
+
+        private void CashierWindowQueue_Load(object sender, EventArgs e)
+        {
+            DisplayCashierQueue(CashierClass.CashierQueue);
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                _tick++;
+                lblTime.Text = _tick.ToString();
+                if (_tick == 15)
+                {
+                    DisplayCashierQueue(CashierClass.CashierQueue.Dequeue());
+                    DisplayCashierQueue(CashierClass.CashierQueue);
+                    timer1.Stop();
+                    _tick = 0;
+                }
+            }
+            catch
+            {
+                timer1.Stop();
+                MessageBox.Show("Theres nothing in the Queue right now.", "Message");
+                _tick = 0;
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
